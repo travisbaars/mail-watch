ef8b88fb96d1f9160c8eb33192bf7b54146d24a7
travisbaars <travis@trainland.io>
2024-02-22 20:30:21 +0000

    Initalize repository

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod auth;
+pub mod service;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+// Read from toml
+// https://codingpackets.com/blog/rust-load-a-toml-file/
+
+// Terminal user input
+// https://users.rust-lang.org/t/how-to-get-user-input/5176/2
+
+// Clear terminal
+// https://docs.rs/clearscreen/latest/clearscreen/index.html
+
+// IMAP crate
+// https://github.com/jonhoo/rust-imap/blob/main/examples/idle.rs
